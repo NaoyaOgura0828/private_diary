1668f3b8075375063290639a5ec6e872e8341c9f
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-06-29 23:34:22 +0900

    auth.dart にファイル名変更 単一ファイルで認証画面を遷移させる

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:private_diary/screens/auth.dart';
+
 
 void main() => runApp(PrivateDiary());
 
---
+++ lib/screens/auth.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('日記投稿アプリ'),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: ,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
