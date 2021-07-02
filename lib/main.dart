83ef7a5efb9bdbb9cf577ec29bd8f24dbe89dc22
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-07-02 15:45:04 +0900

    .dart を main.dart に集約

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
-import 'package:shared_diary/screens/authy.dart';
-import 'package:shared_diary/screens/diary_create.dart';
-import 'package:shared_diary/screens/diary_detail.dart';
-import 'package:shared_diary/screens/diary_list.dart';
-
-import 'package:shared_diary/test.dart';
-
+import 'package:image_picker/image_picker.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,9 +19,290 @@ class SharedDiary extends StatelessWidget {
     return MaterialApp(
       title: '日記投稿アプリ',
       theme: ThemeData.dark(),
-      home: Login(),
-      //home: DiaryCreate(), // デバッグ用
-      //home: DiaryDetail(),
+      home: Aythy(),
+    );
+  }
+}
+
+class Aythy extends StatefulWidget {
+  @override
+  _AythyState createState() => _AythyState();
+}
+
+class _AythyState extends State<Aythy> {
+  // メッセージ表示用
+  String infoText = '';
+
+  // 入力したメールアドレス・パスワード
+  String email = '';
+  String password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('日記投稿アプリ'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // メールアドレス入力
+              TextFormField(
+                decoration: InputDecoration(labelText: 'メールアドレス'),
+                onChanged: (String value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+              ),
+              // パスワード入力
+              TextFormField(
+                decoration: InputDecoration(labelText: 'パスワード'),
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    password = value;
+                  });
+                },
+              ),
+              Container(
+                padding: EdgeInsets.all(8),
+                // メッセージ表示
+                child: Text(infoText),
+              ),
+              Container(
+                width: double.infinity,
+                // ユーザー登録ボタン
+                child: ElevatedButton(
+                  child: Text('ユーザー登録'),
+                  onPressed: () async {
+                    try {
+                      // メール/パスワードでユーザー登録
+                      final FirebaseAuth auth = FirebaseAuth.instance;
+                      await auth.createUserWithEmailAndPassword(
+                        email: email,
+                        password: password,
+                      );
+                      // ユーザー登録に成功した場合
+                      // チャット画面に遷移+ログイン画面を破棄
+                      await Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) {
+                          return DiaryList();
+                        }),
+                      );
+                    } catch (e) {
+                      // ユーザー登録に失敗した場合
+                      setState(() {
+                        infoText = '登録に失敗しました : ${e.toString()}';
+                      });
+                    }
+                  },
+                ),
+              ),
+              const SizedBox(height: 8),
+              Container(
+                width: double.infinity,
+                // ログインボタン
+                child: OutlinedButton(
+                  child: Text('ログイン'),
+                  onPressed: () async {
+                    try {
+                      // メール/パスワードでログイン
+                      final FirebaseAuth auth = FirebaseAuth.instance;
+                      await auth.signInWithEmailAndPassword(
+                        email: email,
+                        password: password,
+                      );
+                      // ログインが成功した場合
+                      // 日記一覧に遷移+ログイン画面を破棄
+                      await Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) {
+                          return DiaryList();
+                        }),
+                      );
+                    } catch (e) {
+                      // ログインに失敗した場合
+                      setState(() {
+                        infoText = 'ログインに失敗しました : ${e.toString()}';
+                      });
+                    }
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DiaryList extends StatefulWidget {
+  const DiaryList({Key? key}) : super(key: key);
+
+  @override
+  _DiaryListState createState() => _DiaryListState();
+}
+
+class _DiaryListState extends State<DiaryList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('日記一覧'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * 3 / 5,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 50),
+                // TODO カード内に日記の情報をimportする
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DiaryCreate extends StatefulWidget {
+  @override
+  _DiaryCreateState createState() => _DiaryCreateState();
+}
+
+class _DiaryCreateState extends State<DiaryCreate> {
+  TextEditingController titleControler =
+      TextEditingController(); // タイトル用コントローラー
+  TextEditingController bodyTextController =
+      TextEditingController(); // 本文用コントローラー
+  final picker = ImagePicker();
+  File? _image;
+
+  Future getImageFromGallery() async {
+    /* ギャラリーから画像を取得 */
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = File(pickedFile!.path);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('日記作成'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'タイトル',
+              ),
+              keyboardType: TextInputType.text,
+              style: TextStyle(fontSize: 20.0),
+              controller: titleControler,
+            ),
+            const SizedBox(height: 10.0),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: '内容',
+              ),
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              style: TextStyle(fontSize: 20.0),
+              controller: bodyTextController,
+            ),
+            Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        width: 100,
+                        child: _image == null
+                            ? Text('写真を選んで下さい')
+                            : Image.file(_image!)),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                FloatingActionButton(
+                  onPressed: getImageFromGallery,
+                  child: Icon(Icons.photo_library),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DiaryList(),
+                        ));
+                  }, // TODO 日記一覧画面に遷移 diary_list
+                  child: Text(
+                    '登録',
+                    style: TextStyle(fontSize: 15.0),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DiaryDetail extends StatefulWidget {
+  @override
+  _DiaryDetailState createState() => _DiaryDetailState();
+}
+
+class _DiaryDetailState extends State<DiaryDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('日記詳細'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text('日付'), // TODO 日付を読み込む
+              Text('タイトル'), // TODO タイトルを読み込む
+              Text('内容'), // TODO 内容を読み込む
+              Text('写真'), // TODO 写真を読み込む
+              ElevatedButton(
+                  onPressed: () => null, // TODO 日記一覧画面に遷移 diary_list
+                  child: Text(
+                    '一覧に戻る',
+                    style: TextStyle(fontSize: 15.0),
+                  )),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
